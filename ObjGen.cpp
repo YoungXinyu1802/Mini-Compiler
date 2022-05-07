9aa0443f14c183ecd61afae296979a994f947e00
flannery-L <3190102370@zju.edu.cn>
2022-05-07 10:48:59 +0800

    init commit

---
+++ ObjGen.cpp
@@ -0,0 +1,59 @@
+#include "CodeGen.h"
+#include "ObjGen.h"
+
+using namespace llvm;
+
+int ObjGen(CodeGenerator& generator)
+{
+    //LLVM提供sys::getDefaultTargetTriple，返回当前计算机的目标三元组。
+    auto TargetTriple = sys::getDefaultTargetTriple();
+    
+    //初始化发出目标代码的所有目标
+    InitializeAllTargetInfos();
+    InitializeAllTargets();
+    InitializeAllTargetMCs();
+    InitializeAllAsmParsers();
+    InitializeAllAsmPrinters();
+
+    std::string Error;
+    auto Target = TargetRegistry::lookupTarget(TargetTriple, Error);
+    // Print an error and exit if we couldn't find the requested target.
+    // This generally occurs if we've forgotten to initialise the
+    // TargetRegistry or we have a bogus target triple.
+    if (!Target) {
+        errs() << Error;
+        return 1;//返回1表示异常退出
+    }
+
+    //使用通用CPU
+    auto CPU = "generic";
+    auto Features = "";
+    TargetOptions opt;
+    auto RM = Optional<Reloc::Model>();
+    auto TargetMachine = Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
+
+    //配置我们的模块，以指定目标和数据布局
+    generator.Module->setDataLayout(TargetMachine->createDataLayout());
+    generator.Module->setTargetTriple(TargetTriple);
+
+    //定义我们要将文件写入的位置：
+    auto Filename = "output.o";
+    std::error_code EC;
+    raw_fd_ostream dest(Filename, EC, sys::fs::F_None);
+    if (EC) {
+        errs() << "Could not open file: " << EC.message();
+        return 1;
+    }
+
+    //定义了一个发出目标代码的传递，然后我们运行该传递
+    legacy::PassManager pass;
+    auto FileType = TargetMachine::CGFT_ObjectFile;
+    if (TargetMachine->addPassesToEmitFile(pass, dest, FileType)) {
+        errs() << "TargetMachine can't emit a file of this type";
+        return 1;
+    }
+    pass.run(*(generator.module));
+    dest.flush();
+
+    return 0;
+}
---
+++ ObjGen.h
@@ -0,0 +1,19 @@
+#ifndef OBJGEN_H
+#define OBJGEN_H
+
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/Host.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/TargetRegistry.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Target/TargetMachine.h>
+#include <llvm/Target/TargetOptions.h>
+#include <llvm/ADT/Optional.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/FormattedStream.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/IR/LegacyPassManager.h>
+
+int ObjGen(CodeGenerator& generator);
+
+#endif 
